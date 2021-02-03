1ec18deeca2f03214f964941e7447bd885601f6e
Lucas Dias de Almeida <lucasdias1589@gmail.com>
2021-02-03 16:20:16 -0300

    Anonymous class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
-import util.MyComparator;
 
 public class Program {
 
@@ -16,7 +16,15 @@ public class Program {
 		products.add(new Product("Tablet", 4000.00));
 		products.add(new Product("Computer", 5000.00));
 		
-		products.sort(new MyComparator());
+		Comparator<Product> comp = new Comparator<Product>() {
+		
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getPrice().compareTo(p2.getPrice());
+			}
+		};
+		
+		products.sort(comp);
 							
 		
 		for(Product p : products) {
